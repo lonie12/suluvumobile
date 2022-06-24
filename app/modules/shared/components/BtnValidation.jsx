0035967e32dad0e9edaa5a965d3c008573f15d9e
LonieCre <rbatoulime@gmail.com>
2022-06-24 14:39:54 +0000

    ButtonValidation as BtnValidation component

---
+++ app/modules/shared/components/BtnValidation.jsx
@@ -0,0 +1,26 @@
+import {StyleSheet, TouchableOpacity, Text} from 'react-native';
+import { buttonTextColor, mainColor } from '../utils/colors';
+
+export default (props) => {
+
+    return (
+        <TouchableOpacity style={{...styles.touchable, ...props.style}} onPress={props.validate}>
+            <Text style={{...styles.text, ...props.textStyle}}> {props.title} </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    touchable: {
+        backgroundColor: mainColor,
+        paddingVertical: 10,
+        borderRadius: 10
+    },
+
+    text: {
+        color: buttonTextColor,
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: '500'
+    }
+})
